61784833097888e140575fbb54a225c22952e117
SovietDuke <kozlovegoryur@gmail.com>
2023-10-04 23:16:12 +0400

    Добавил заголовочные файлы

--- Array.h
+++ Array.h
@@ -1,1 +1,37 @@
 #pragma once
+
+template <typename T> class Array
+{
+public:
+	Array() = default;
+
+	Array(int length);
+
+	Array(const Array& Array);
+
+	virtual ~Array();
+
+	void erase();
+
+	void reallocate(int newLength);
+
+	void resize(int newLength);
+
+	int& operator [](int i);
+
+	int getLength() const;
+
+	Array& operator=(const Array& Array);
+
+	void insertBefore(T value, int i);
+
+	void remove(int i);
+
+	void insertAtBeginning(T value);
+
+	void insertAtEnd(T value);
+
+private:
+	int m_length{};
+	T* m_data{};
+};
